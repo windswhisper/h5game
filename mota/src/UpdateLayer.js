02a4a27ae8beb8e58723301facf8d7ffcdf6f114
邓浩 <DengHao@denghaodeMacBook-Pro.local>
2017-10-07 00:45:37 +0800

    mota

--- mota/src/UpdateLayer.js
+++ mota/src/UpdateLayer.js
@@ -3,6 +3,8 @@ var UpdateLayer = cc.Node.extend({
 	{
 		this._super();
 
+		var self = this;
+
 		var bg = cc.Sprite.create("res/black.png");
 		bg.setPosition(135,172);
 		bg.setOpacity(200);
@@ -24,6 +26,37 @@ var UpdateLayer = cc.Node.extend({
 		hpbtn.setPosition(135,120);
 		this.addChild(hpbtn);
 
+		cc.eventManager.addListener({
+        	event: cc.EventListener.TOUCH_ONE_BY_ONE,
+        	swallowTouches: true,   
+        	onTouchBegan: function (touch, event) {
+        		var p = self.convertToNodeSpace(touch.getLocation()); 
+
+        		if(cc.rectContainsPoint(atkbtn.getBoundingBox(),p))
+        			self.select(1);
+        		if(cc.rectContainsPoint(defbtn.getBoundingBox(),p))
+        			self.select(2);
+        		if(cc.rectContainsPoint(hpbtn.getBoundingBox(),p))
+        			self.select(3);
+        		return true;
+        	}
+    	}, this);
+
+	 	var listener1 = cc.EventListener.create({
+	        event: cc.EventListener.TOUCH_ONE_BY_ONE,
+	        swallowTouches: true, 
+	        onTouchBegan: function (touch, event) {
+        		var p = self.convertToNodeSpace(touch.getLocation()); 
+        		if(cc.rectContainsPoint(atkbtn.getBoundingBox(),p))
+        			self.select(1);
+        		if(cc.rectContainsPoint(defbtn.getBoundingBox(),p))
+        			self.select(2);
+        		if(cc.rectContainsPoint(hpbtn.getBoundingBox(),p))
+        			self.select(3);
+        		return true;
+        	}
+	    });
+	    cc.eventManager.addListener(listener1, this);
 		cc.eventManager.addListener({     
         event: cc.EventListener.KEYBOARD,     
         onKeyPressed:  this.keyDown, 
@@ -35,10 +68,26 @@ var UpdateLayer = cc.Node.extend({
 		event.stopPropagation();
 		switch(keyCode)
 		{
-			case 49:_heroInfo.atk+=3;target.removeFromParent();break;
-			case 50:_heroInfo.def+=3;target.removeFromParent();break;
-			case 51:_heroInfo.maxhp+=40;_heroInfo.hp = _heroInfo.maxhp;target.removeFromParent();break;
+			case 49:target.select(1);break;
+			case 50:target.select(2);break;
+			case 51:target.select(3);break;
+		}
+	},
+	select:function(option){
+		switch(option)
+		{
+			case 1:_heroInfo.atk+=3;this.remove();break;
+			case 2:_heroInfo.def+=3;this.remove();break;
+			case 3:_heroInfo.maxhp+=40;_heroInfo.hp = _heroInfo.maxhp;this.remove();break;
 		}
 		_uilayer.showInfo();
+	},
+	remove:function()
+	{
+		var self = this;
+		this.schedule(function(){
+			self.removeFromParent();
+		}.bind(this),0);
 	}
+
 });

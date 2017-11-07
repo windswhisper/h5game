04df588b017b5066f231ab84b40af111eba784af
邓浩 <DengHao@denghaodeMacBook-Pro.local>
2017-11-07 22:35:44 +0800

    stars

---
+++ star/src/GameLayer.js
@@ -0,0 +1,140 @@
+var BOARD_SIZE = cc.size(9,9);
+var BLOCK_SIZE = cc.size(55,55);
+function inRange(x,y)
+{
+	return x<=BOARD_SIZE.width-1&&x>=0&&y<=BOARD_SIZE.height-1&&y>=0;
+}
+var GameLayer = cc.Layer.extend({
+	boardNode:null,
+	beansArray:[],
+	spriteArray:[],
+	countTempArray:[],
+	count:null,
+	ctor:function()
+	{
+		this._super();
+
+		for(var i=0;i<BOARD_SIZE.width;i++)
+		{
+			this.beansArray[i] = new Array();
+			this.spriteArray[i] = new Array();
+			this.countTempArray[i] = new Array();
+		}
+		this.boardNode = cc.Node.create();
+		this.boardNode.setPosition(45,45);
+		this.addChild(this.boardNode);
+		this.newGame();
+
+        cc.eventManager.addListener({
+          event: cc.EventListener.TOUCH_ONE_BY_ONE,
+          swallowTouches: false,
+          onTouchBegan: this.onTouchBegan,
+          onTouchMoved: this.onTouchMoved,
+          onTouchEnded: this.onTouchEnded
+        }, this);
+	},
+	newGame:function()
+	{
+		for(var i=0;i<BOARD_SIZE.width;i++)
+		{
+			for(var j=0;j<BOARD_SIZE.height;j++)
+			{
+				this.putBall(i,j);
+			}
+		}
+	},
+	putBall:function(x,y)
+	{
+		this.beansArray[x][y] = Math.floor(Math.random()*4)+1;
+		this.spriteArray[x][y] = new Bean("res/"+this.beansArray[x][y]+".png");
+		this.spriteArray[x][y].setPosition(x*BLOCK_SIZE.width,y*BLOCK_SIZE.height);
+		this.boardNode.addChild(this.spriteArray[x][y]);
+	},
+    onTouchBegan:function(touch, event) 
+    {
+        var target = event.getCurrentTarget();
+        var p = touch.getLocation();
+
+        return true;
+    },
+    onTouchMoved:function(touch, event) 
+    {
+        var target = event.getCurrentTarget();
+        var p = touch.getLocation();
+
+    },
+    onTouchEnded:function(touch, event) 
+    {
+        var target = event.getCurrentTarget();
+        var p = target.boardNode.convertTouchToNodeSpace(touch);
+
+        p.x += BLOCK_SIZE.width/2;
+        p.y += BLOCK_SIZE.height/2;
+
+        var x= Math.floor(p.x/BLOCK_SIZE.width);
+        var y= Math.floor(p.y/BLOCK_SIZE.height);
+
+        target.clickxy(x,y);
+    },
+    clickxy:function(x,y)
+    {
+    	this.count = 0;
+		for(var i=0;i<BOARD_SIZE.width;i++)
+			for(var j=0;j<BOARD_SIZE.height;j++)
+    			this.countTempArray[i][j] = 0;
+    	this.countNear(x,y);
+    	if(this.count>=2)
+    	{
+			for(var i=0;i<BOARD_SIZE.width;i++)
+				for(var j=0;j<BOARD_SIZE.height;j++)
+	    			if(this.countTempArray[i][j] != 0)
+	    				{
+	    					this.hitBean(i,j);
+	    					this.putBall(i,j);
+	    				}
+    	}
+    },
+    countNear:function(x,y){
+    	this.count++;
+    	this.countTempArray[x][y]=1;
+    	if(inRange(x+1,y)&&this.countTempArray[x+1][y]==0&&this.beansArray[x+1][y]==this.beansArray[x][y]){
+    		this.countNear(x+1,y);
+    	}
+    	if(inRange(x-1,y)&&this.countTempArray[x-1][y]==0&&this.beansArray[x-1][y]==this.beansArray[x][y]){
+    		this.countNear(x-1,y);
+    	}
+    	if(inRange(x,y+1)&&this.countTempArray[x][y+1]==0&&this.beansArray[x][y+1]==this.beansArray[x][y]){
+    		this.countNear(x,y+1);
+    	}
+    	if(inRange(x,y-1)&&this.countTempArray[x][y-1]==0&&this.beansArray[x][y-1]==this.beansArray[x][y]){
+    		this.countNear(x,y-1);
+    	}
+    },
+    hitBean:function(x,y)
+    {
+		this.spriteArray[x][y].hit();
+		this.beansArray[x][y] = 0;
+    }
+});
+
+var Bean = cc.Sprite.extend({
+	ctor:function(picName)
+	{
+		this._super(picName);
+		this.setScale(0);
+		this.runAction(new cc.Sequence(new cc.DelayTime(0.8),new cc.ScaleTo(0.2,1)));
+	},
+	hit:function()
+	{
+		this.setZOrder(2);
+		this.vx = Math.random()*8-4;
+		this.vy = Math.random()*6+4;
+		this.scheduleUpdate();
+	},
+	update:function(dt)
+	{
+		this.setPosition(this.getPosition().x+this.vx,this.getPosition().y+this.vy);
+		this.vy-=1;
+		if(this.getPosition().y<-300)this.removeFromParent();
+	}
+});
---
+++ star/src/GameScene.js
@@ -0,0 +1,25 @@
+var GameScene = cc.Scene.extend({
+	ctor:function()
+	{
+		this._super();
+
+		this.addChild(new GameLayer());
+
+		this.adaptScreen();
+	},
+	adaptScreen:function()
+	{
+	 	cc.view.setDesignResolutionSize(540,960,cc.ResolutionPolicy.NO_BORDER);
+
+		var size = cc.director.getWinSize();
+        var visibleSize = cc.view.getVisibleSize();
+        this.setPosition(size.width/2-visibleSize.height/size.height*size.width/2,size.height/2-visibleSize.height/size.height*size.height/2);
+        this.setAnchorPoint(0, 0);
+        this.setScale(visibleSize.height/size.height);
+
+        var rect = cc.DrawNode.create();
+        rect.drawRect(cc.p(2,2),cc.p(538,958));
+        rect.setPosition(0,0);
+        this.addChild(rect);
+	}
+});

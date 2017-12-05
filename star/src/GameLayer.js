8123f7718cd22442a6f968d01c8fa5d9f2810327
邓浩 <DengHao@denghaodeMacBook-Pro.local>
2017-12-06 00:02:39 +0800

    SCORE

--- star/src/GameLayer.js
+++ star/src/GameLayer.js
@@ -213,7 +213,7 @@ var GameLayer = cc.Layer.extend({
 		this.bottomView.addChild(progressBg); 
 
 		this.progressBar = new cc.Sprite("res/playpage_progress.png");
-		this.progressBar.setPosition(540,140);
+		this.progressBar.setPosition(540,125);
 		this.bottomView.addChild(this.progressBar); 
 
 		this.btnPause = new cc.Sprite("res/btn_pause.png");
@@ -603,8 +603,8 @@ var GameLayer = cc.Layer.extend({
     	if(this.powerDisplay>1)this.powerDisplay=1;
     	if(this.powerDisplay<0)this.powerDisplay=0;
 
-		this.progressBar.setPosition(540-828/2*(1-this.powerDisplay),140);
-		this.progressBar.setTextureRect(cc.rect(0,0,828*this.powerDisplay,105));
+		this.progressBar.setPosition(540-818/2*(1-this.powerDisplay),125);
+		this.progressBar.setTextureRect(cc.rect(0,0,818*this.powerDisplay,100));
     },
     getScore:function(s)
     {
